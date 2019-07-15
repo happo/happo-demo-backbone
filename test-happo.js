57fdbc02ddf793f7441fa3780c2fa3c9553df390
Henric Trotzig <henric.trotzig@gmail.com>
2019-07-15 19:28:33 +0200

    Add POC backbone rendering

--- test-happo.js
+++ test-happo.js
@@ -1,3 +1,16 @@
+import Backbone from 'backbone';
+
+var AppView = Backbone.View.extend({
+  el: '#happo-root',
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    this.$el.html('<div>Hello World</div>');
+  },
+});
+
 export default () => {
-  document.body.innerHTML = '<div>Hello world</div>';
+  new AppView();
+  return new Promise(r => setTimeout(r, 0));
 };
